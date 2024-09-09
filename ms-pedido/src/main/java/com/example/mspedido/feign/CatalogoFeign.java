152b60f47d5f76ea86fc3db6f3f95f7ff260b681
enders9245 <juanenderprietoquispe@gmail.com>
2024-09-09 10:53:20 -0500

    a

--- ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="ms-catalogo-service",path = "/producto")
+//@FeignClient(name="ms-catalogo-service",path = "/producto")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
     @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackProducto")
